85b05a13372da8b8c024d25e21c95a48e79d97c7
Aryan Roy <aryanroy@seas.upenn.edu>
2024-09-22 01:08:22 +0000

    lab - 1

--- lib/calculateFK.py
+++ lib/calculateFK.py
@@ -1,15 +1,34 @@
 import numpy as np
-from math import pi
+from math import pi, sin, cos
 
 class FK():
 
     def __init__(self):
-
-        # TODO: you may want to define geometric parameters here that will be
-        # useful in computing the forward kinematics. The data you will need
-        # is provided in the lab handout
-
-        pass
+        self.dh_table = [
+            [0,      -pi/2, 0.333,  0],
+            [0,       pi/2, 0,      0],
+            [0.0825,  pi/2, 0.316,  0],
+            [-0.0825, -pi/2, 0,     0],
+            [0,       pi/2, 0.384,  0],
+            [0.088,   pi/2, 0,      0],
+            [0,       0,    0.210,  -pi/4]
+        ]
+        self.joint_offsets = {
+            2: np.array([0, 0, 0.195]),
+            4: np.array([0, 0, 0.125]),
+            5: np.array([0, 0, -0.015]),
+            6: np.array([0, 0, 0.051])
+        }
+        self.base_elevation = 0.141  # Base elevation in meters
+        
+    def dh_transform(self, a, d, alpha, theta):
+        """Compute the homogeneous transformation matrix based on DH parameters."""
+        return np.array([
+            [cos(theta), -sin(theta)*cos(alpha),  sin(theta)*sin(alpha), a*cos(theta)],
+            [sin(theta),  cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta)],
+            [0,           sin(alpha),             cos(alpha),            d           ],
+            [0,           0,                      0,                     1           ]
+        ])
 
     def forward(self, q):
         """
@@ -17,62 +36,47 @@ class FK():
         q - 1x7 vector of joint angles [q0, q1, q2, q3, q4, q5, q6]
 
         OUTPUTS:
-        jointPositions -8 x 3 matrix, where each row corresponds to a rotational joint of the robot or end effector
-                  Each row contains the [x,y,z] coordinates in the world frame of the respective joint's center in meters.
-                  The base of the robot is located at [0,0,0].
-        T0e       - a 4 x 4 homogeneous transformation matrix,
-                  representing the end effector frame expressed in the
-                  world frame
+        jointPositions - 8 x 3 matrix, where each row corresponds to a rotational joint of the robot or end effector
+        T0e            - a 4 x 4 homogeneous transformation matrix, representing the end effector frame in the world frame
         """
+        jointPositions = np.zeros((8, 3))
+        T = np.eye(4)
+        
+        for i in range(7):  # For each joint
+            a, alpha, d, theta_offset = self.dh_table[i]
+            theta = q[i] + theta_offset
+            Ti = self.dh_transform(a, d, alpha, theta)
+            T = T @ Ti
+            
+            # Store the coordinate frame origin
+            coord_frame_origin = T[:3, 3]
+            
+            # Apply joint offset if it exists
+            if i+1 in self.joint_offsets:
+                # Transform the local offset to world frame
+                world_offset = T[:3, :3] @ self.joint_offsets[i+1]
+                jointPositions[i+1] = coord_frame_origin + world_offset
+            else:
+                jointPositions[i+1] = coord_frame_origin
 
-        # Your Lab 1 code starts here
-
-        jointPositions = np.zeros((8,3))
-        T0e = np.identity(4)
-
-        # Your code ends here
+        # Add base elevation only to the first joint position
+        jointPositions[0, 2] += self.base_elevation
+        
+        # The end-effector transformation doesn't change
+        T0e = T
 
         return jointPositions, T0e
 
-    # feel free to define additional helper methods to modularize your solution for lab 1
-
-    
-    # This code is for Lab 2, you can ignore it ofr Lab 1
     def get_axis_of_rotation(self, q):
-        """
-        INPUT:
-        q - 1x7 vector of joint angles [q0, q1, q2, q3, q4, q5, q6]
+        raise NotImplementedError("This method is not implemented yet.")
 
-        OUTPUTS:
-        axis_of_rotation_list: - 3x7 np array of unit vectors describing the axis of rotation for each joint in the
-                                 world frame
-
-        """
-        # STUDENT CODE HERE: This is a function needed by lab 2
-
-        return()
-    
     def compute_Ai(self, q):
-        """
-        INPUT:
-        q - 1x7 vector of joint angles [q0, q1, q2, q3, q4, q5, q6]
+        raise NotImplementedError("This method is not implemented yet.")
 
-        OUTPUTS:
-        Ai: - 4x4 list of np array of homogenous transformations describing the FK of the robot. Transformations are not
-              necessarily located at the joint locations
-        """
-        # STUDENT CODE HERE: This is a function needed by lab 2
-
-        return()
-    
 if __name__ == "__main__":
-
     fk = FK()
-
-    # matches figure in the handout
-    q = np.array([0,0,0,-pi/2,0,pi/2,pi/4])
-
+    q = np.array([0, 0, 0, -pi/2, 0, pi/2, pi/4])
     joint_positions, T0e = fk.forward(q)
     
-    print("Joint Positions:\n",joint_positions)
-    print("End Effector Pose:\n",T0e)
+    print("Joint Positions:\n", joint_positions)
+    print("End Effector Pose:\n", T0e)
